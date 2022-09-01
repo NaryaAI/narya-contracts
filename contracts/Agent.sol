ce0a60206ae712da7fe3cdfa1924e660ab09a919
tarafans <tarafans7@gmail.com>
2022-09-01 09:47:40 -0400

    v1.0.7

--- contracts/Agent.sol
+++ contracts/Agent.sol
@@ -1,43 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./forge-std/Test.sol";
+
 struct Call {
     address to;
     bytes callData;
 }
 
-contract Agent {
+contract Agent is Test {
+    event RevertedCall(bytes4 selector, uint256 index);
+
+    bytes4 internal constant MAIN = bytes4(0x41414141);
+    bytes4 internal constant FALLBACK = bytes4(0x00000000);
+    bytes4 internal constant ON_ERC721_RECEIVED = bytes4(0x150b7a02);
+
     function main(Call[] calldata calls) external {
-        for (uint256 i = 0; i < calls.length; i++) {
+        uint256 i;
+        for (i = 0; i < calls.length; i++) {
             (bool success, ) = calls[i].to.call(calls[i].callData);
             if (success) {
                 this.check();
             } else {
+                emit RevertedCall(MAIN, i);
                 return;
             }
         }
     }
 
-    function _callback(Call[] calldata calls) internal {
-        for (uint256 i = 0; i < calls.length; i++) {
+    function _callback(bytes4 selector, Call[] calldata calls) internal {
+        uint256 i;
+        for (i = 0; i < calls.length; i++) {
             (bool success, ) = calls[i].to.call(calls[i].callData);
             if (!success) {
+                emit RevertedCall(selector, i);
                 return;
             }
         }
     }
 
     function onERC721Received(Call[] calldata calls) external returns (bytes4) {
-        _callback(calls);
-        return bytes4(0x150b7a02);
+        _callback(ON_ERC721_RECEIVED, calls);
+        return ON_ERC721_RECEIVED;
     } 
 
     function fallback(Call[] calldata calls) external payable {
-        _callback(calls);
+        _callback(FALLBACK, calls);
     }
 
-    function callback(Call[] calldata calls) external {
-        _callback(calls);
+    function callback(bytes4 selector, Call[] calldata calls) external {
+        _callback(selector, calls);
     }
 
     function check() external virtual {}
