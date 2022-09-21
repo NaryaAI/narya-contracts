694113cfc9cff0d5f2d0ffb41d04780354d4ddff
Ranmocy <ranmocy@gmail.com>
2022-09-20 22:25:34 -0400

    Add example-foundry

---
+++ example-foundry/test/pnm/TargetV1.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@pwnednomore/contracts/Agent.sol";
+// Notice that Target contract uses hardhat console
+// which is conflict with our Agent contract's console.
+// So we need to explicitly import symbol here to
+// avoid importing two console contracts to the global namespace.
+import {Target} from "src/Target.sol";
+
+contract TargetTest is Agent {
+    Target target;
+
+    function setUp() public {
+        target = new Target();
+    }
+
+    function test() external {
+        require(!target.stolen(), "stolen");
+        console.log("Test success");
+    }
+}
---
+++ example-foundry/test/pnm/TargetV2_parameters.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@pwnednomore/contracts/FuzzParameterTest.sol";
+import {Target} from "src/Target.sol";
+
+contract TargetParameterTest is FuzzParameterTest {
+    address user;
+    Target target;
+
+    function setUp() public {
+        user = 0x0000000000111111111100000000001111111111;
+        target = new Target();
+
+        // Call this function is you want to discard state changes after each test
+        // vm.enableTestSideEffect(false);
+    }
+
+    // This function will be called again and again, with intellegently selected random data
+    function test_paintColorAgainAndAgain(string memory color) external {
+        target.paint(color);
+        require(!target.stolen(), "TestReport: Door is stolen!");
+    }
+}
