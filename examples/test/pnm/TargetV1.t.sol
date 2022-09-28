13eb1eade192aecef74805fbc728efb071f721c5
Ranmocy <ranmocy@gmail.com>
2022-09-27 20:57:45 -0400

    Simplify examples

--- examples/test/pnm/TargetV1.t.sol
+++ examples/test/pnm/TargetV1.t.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Notice that if target contract uses hardhat console
+// it could conflict with our Agent contract's console.
+// You have two options:
+// 1. Use our console version everywhere
+// 2. Use explicit symbol import here like `import {Symbol} from "contract";`
 import "@pwnednomore/contracts/Agent.sol";
-// Notice that Target contract uses hardhat console
-// which is conflict with our Agent contract's console.
-// So we need to explicitly import symbol here to
-// avoid importing two console contracts to the global namespace.
-import {Target} from "src/Target.sol";
+import "src/Target.sol";
 
 contract TargetTest is Agent {
     Target target;
