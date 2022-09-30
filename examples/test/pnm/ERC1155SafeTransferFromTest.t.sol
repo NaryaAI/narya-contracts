cd3256fc1861cd56b60631293672a2923476055b
Conan <conanchou1412@gmail.com>
2022-09-29 22:58:24 -0400

    add ERC 1155 invariant

---
+++ examples/test/pnm/ERC1155SafeTransferFromTest.t.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@pwnednomore/contracts/Agent.sol";
+import "src/GameItems.sol";
+
+contract ERC1155SafeTransferFromTest is Agent {
+    address alice = address(0x927);
+    GameItems gameItems;
+    uint initAmount = 50;
+
+    function setUp() public {
+        address owner = address(0x1);
+        vm.startPrank(owner);
+        gameItems = new GameItems();
+        gameItems.safeBatchTransferFrom(
+            owner, 
+            alice, 
+            [GameItems.GOLD, GameItems.SILVER], 
+            [initAmount, initAmount], 
+            ""
+        );
+        vm.stopPrank();
+    }
+
+    function testSafeTransferFrom(bool approved, uint id, uint amount) public {
+        uint aliceBalance = gameItems.balanceOf(alice, id);
+        uint agentBalance = gameItems.balanceOf(address(this), id);
+
+        vm.prank(alice);
+        gameItems.setApprovalForAll(address(this), approved);
+
+        gameItems.safeTransferFrom(alice, address(this), id, amount, "");
+
+        assert(approved);
+        assert(amount <= initAmount);
+        assert(id == GameItems.GOLD || id == GameItems.SILVER);
+        assert(gameItems.balanceOf(alice, id) == aliceBalance - amount);
+        assert(gameItems.balanceOf(address(this), id) == agentBalance + amount);
+    }
+}
