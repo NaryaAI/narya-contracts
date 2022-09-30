6db5d5d10cc6ba985d7e48e8ededdbde6757fcf7
Conan <conanchou1412@gmail.com>
2022-09-29 23:11:32 -0400

    simplify ERC20TransferFromTest

--- examples/test/pnm/ERC20TransferFromTest.t.sol
+++ examples/test/pnm/ERC20TransferFromTest.t.sol
@@ -6,30 +6,30 @@ import "@pwnednomore/contracts/Agent.sol";
 import "src/Token.sol";
 
 contract ERC20TransferFromTest is Agent {
-    address alice;
-    address bob;
+    address alice = address(0x927);
     Token token;
 
     function setUp() public {
+        address owner = address(0x1);
+        vm.startPrank(owner);
         token = new Token();
-        alice = address(0x927);
-        bob = address(0x928);
         token.transfer(alice, 50);
-        token.transfer(bob, 50);
+        vm.stopPrank();
+
         vm.prank(alice);
         token.approve(address(this), 20);
     }
 
     function testTransferFrom(uint amount) public {
         uint aliceBalance = token.balanceOf(alice);
-        uint bobBalance = token.balanceOf(bob);
+        uint agentBalance = token.balanceOf(address(this));
         uint allowance = token.allowance(alice, address(this));
 
-        token.transferFrom(alice, bob, amount);
+        token.transferFrom(alice, address(this), amount);
 
         assert(amount <= allowance);
         assert(token.balanceOf(alice) == aliceBalance - amount);
-        assert(token.balanceOf(address(this)) == bobBalance + amount);
+        assert(token.balanceOf(address(this)) == agentBalance + amount);
         assert(token.allowance(alice, address(this)) == allowance - amount);
     }
 }
