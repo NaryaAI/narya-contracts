1b1fdbb67802780da2d442b7bc6f4c475d3e440d
Ranmocy <ranmocy@gmail.com>
2022-09-30 15:52:38 -0400

    Fix VmHelpers

--- contracts/VmEx.sol
+++ contracts/VmEx.sol
@@ -4,59 +4,59 @@ pragma solidity >=0.6.0 <0.9.0;
 import "./forge-std/Vm.sol";
 import "./VmHelpers.sol";
 
-interface VmEx is Vm, VmHelpers {
+abstract contract VmEx is Vm, VmHelpers {
 
     // read int256 variable by name
-    function readInt(address target, string calldata varName) external view returns (int256);
+    function readInt(address target, string calldata varName) virtual external view returns (int256);
     // read int256 variable by slot index
-    function readIntBySlot(address target, uint256 index) external view returns (int256);
+    function readIntBySlot(address target, uint256 index) virtual external view returns (int256);
     // read int8 varriable by name
-    function readInt8(address target, string calldata varName) external view returns (int8);
+    function readInt8(address target, string calldata varName) virtual external view returns (int8);
     // read int8 variable by slot index and offset
-    function readInt8BySlot(address target, uint256 index, uint256 offset) external view returns (int8);
+    function readInt8BySlot(address target, uint256 index, uint256 offset) virtual external view returns (int8);
 
     // read uint256 variable by name
-    function readUint(address target, string calldata varName) external view returns (uint256);
+    function readUint(address target, string calldata varName) virtual external view returns (uint256);
     // read uint8 variable by slot index
-    function readUintBySlot(address target, uint256 index) external view returns (uint256);
+    function readUintBySlot(address target, uint256 index) virtual external view returns (uint256);
     // read uint8 variable by name
-    function readUint8(address target, string calldata varName) external view returns (uint8);
+    function readUint8(address target, string calldata varName) virtual external view returns (uint8);
     // read uint8 by slot index and offset
-    function readUint8BySlot(address target, uint256 index, uint256 offset) external view returns (uint8);
+    function readUint8BySlot(address target, uint256 index, uint256 offset) virtual external view returns (uint8);
 
     // read address variable by name
-    function readAddress(address target, string calldata varName) external view returns (address);
+    function readAddress(address target, string calldata varName) virtual external view returns (address);
     // read address by slot index and offset
-    function readAddressBySlot(address target, uint256 index, uint256 offset) external view returns (address);
+    function readAddressBySlot(address target, uint256 index, uint256 offset) virtual external view returns (address);
 
     // read raw `size` bytes by slot index and slot offset
-    function readRawBytes(address target, uint256 index, uint256 offset, uint256 size) external view returns (bytes memory);
+    function readRawBytes(address target, uint256 index, uint256 offset, uint256 size) virtual external view returns (bytes memory);
     // read `size` bytes by variable name
-    function readBytes(address target, string calldata varName, uint256 size) external view returns (bytes memory);
+    function readBytes(address target, string calldata varName, uint256 size) virtual external view returns (bytes memory);
 
     // read whole int8 array data by name
-    function readInt8Array(address target, string calldata varName) external view returns (int8[] memory);
+    function readInt8Array(address target, string calldata varName) virtual external view returns (int8[] memory);
     // read int8 array element at index by name
-    function readInt8ArrayElem(address target, string calldata varName, uint256 index) external view returns (int8);
+    function readInt8ArrayElem(address target, string calldata varName, uint256 index) virtual external view returns (int8);
     // read whole int256 array data by name
-    function readInt256Array(address target, string calldata varName) external view returns (int256[] memory);
+    function readInt256Array(address target, string calldata varName) virtual external view returns (int256[] memory);
     // read int256 array element at index by name
-    function readInt256ArrayElem(address target, string calldata varName, uint256 index) external view returns (int256);
+    function readInt256ArrayElem(address target, string calldata varName, uint256 index) virtual external view returns (int256);
     // read whole uint8 array data by name
-    function readUint8Array(address target, string calldata varName) external view returns (uint8[] memory);
+    function readUint8Array(address target, string calldata varName) virtual external view returns (uint8[] memory);
     // read uint8 array element at index by name
-    function readUint8ArrayElem(address target, string calldata varName, uint256 index) external view returns (uint8);
+    function readUint8ArrayElem(address target, string calldata varName, uint256 index) virtual external view returns (uint8);
     // read whole uint256 array data by name
-    function readUint256Array(address target, string calldata varName) external view returns (uint256[] memory);
+    function readUint256Array(address target, string calldata varName) virtual external view returns (uint256[] memory);
     // read uint256 array element at index by name
-    function readUint256ArrayElem(address target, string calldata varName, uint256 index) external view returns (uint256);
+    function readUint256ArrayElem(address target, string calldata varName, uint256 index) virtual external view returns (uint256);
 
-    function readAddressUint256MapValue(address target, string calldata varName, address key) external view returns (uint256);
-    function readUint256Uint256MapValue(address target, string calldata varName, uint256 key) external view returns (uint256);
+    function readAddressUint256MapValue(address target, string calldata varName, address key) virtual external view returns (uint256);
+    function readUint256Uint256MapValue(address target, string calldata varName, uint256 key) virtual external view returns (uint256);
 
     // get 1D Array[index] slot id by variable name
-    function getArraySlotIndex(address target, string calldata varName, uint256 index) external view returns (uint256);
+    function getArraySlotIndex(address target, string calldata varName, uint256 index) virtual external view returns (uint256);
 
     // get slot id by variable name
-    function getVarSlotIndex(address target, string calldata varName) external view returns (uint256);
+    function getVarSlotIndex(address target, string calldata varName) virtual external view returns (uint256);
 }
--- contracts/VmHelpers.sol
+++ contracts/VmHelpers.sol
@@ -3,26 +3,26 @@ pragma solidity >=0.6.0 <0.9.0;
 
 import "./forge-std/Vm.sol";
 
-interface VmHelpers is Vm {
+abstract contract VmHelpers is Vm {
 
     // Sets the *next* call's msg.sender to be the input address
     function asAccountForNextCall(address addr) external {
-        prank(addr);
+        this.prank(addr);
     }
     // Sets the *next* call's msg.sender to be the input address, and the tx.origin to be the second input
     function asAccountForNextCall(address addr, address origin) external {
-        prank(addr, origin);
+        this.prank(addr, origin);
     }
     // Sets all subsequent calls' msg.sender to be the input address until `stopPrank` is called
     function asAccountBegin(address addr) external {
-        startPrank(addr);
+        this.startPrank(addr);
     }
     function asAccountBegin(address addr, address origin) external {
-        startPrank(addr, origin);
+        this.startPrank(addr, origin);
     }
     // Resets subsequent calls' msg.sender to be `address(this)`
     function asAccountEnd() external {
-        stopPrank();
+        this.stopPrank();
     }
 
 }
