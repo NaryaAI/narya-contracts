a0ff3fde90fb90184091552040f4bb77ac683145
Ranmocy <ranmocy@gmail.com>
2022-09-30 14:32:56 -0400

    Fix example complilation

--- examples/test/pnm/ERC1155SafeTransferFromTest.t.sol
+++ examples/test/pnm/ERC1155SafeTransferFromTest.t.sol
@@ -7,26 +7,30 @@ import "src/GameItems.sol";
 contract ERC1155SafeTransferFromTest is Agent {
     address alice = address(0x927);
     GameItems gameItems;
-    uint initAmount = 50;
+    uint256 initAmount = 50;
 
     function setUp() public {
         address owner = address(0x1);
         vm.startPrank(owner);
         gameItems = new GameItems();
-        gameItems.safeBatchTransferFrom(
-            owner, 
-            alice, 
-            [GameItems.Item.GOLD, GameItems.Item.SILVER], 
-            [initAmount, initAmount], 
-            ""
-        );
+        uint256[] memory items = new uint256[](2);
+        items[0] = uint256(GameItems.Item.GOLD);
+        items[1] = uint256(GameItems.Item.SILVER);
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0] = initAmount;
+        amounts[1] = initAmount;
+        gameItems.safeBatchTransferFrom(owner, alice, items, amounts, "");
         vm.stopPrank();
     }
 
-    function testSafeTransferFrom(bool approved, GameItems.Item item, uint amount) public {
-        uint id = uint(item);
-        uint aliceBalance = gameItems.balanceOf(alice, id);
-        uint agentBalance = gameItems.balanceOf(address(this), id);
+    function testSafeTransferFrom(
+        bool approved,
+        GameItems.Item item,
+        uint256 amount
+    ) public {
+        uint256 id = uint256(item);
+        uint256 aliceBalance = gameItems.balanceOf(alice, id);
+        uint256 agentBalance = gameItems.balanceOf(address(this), id);
 
         vm.prank(alice);
         gameItems.setApprovalForAll(address(this), approved);
@@ -35,7 +39,10 @@ contract ERC1155SafeTransferFromTest is Agent {
 
         assert(approved);
         assert(amount <= initAmount && amount <= aliceBalance);
-        assert(id == unit(GameItems.GOLD) || id == uint(GameItems.SILVER));
+        assert(
+            id == uint256(GameItems.Item.GOLD) ||
+                id == uint256(GameItems.Item.SILVER)
+        );
         assert(gameItems.balanceOf(alice, id) == aliceBalance - amount);
         assert(gameItems.balanceOf(address(this), id) == agentBalance + amount);
     }
