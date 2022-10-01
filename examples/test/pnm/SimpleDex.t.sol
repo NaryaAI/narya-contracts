682062b1469b9815fe25a268d11961a565d0fbaf
Ranmocy <ranmocy@gmail.com>
2022-09-30 22:16:05 -0400

    Update simple dex test

--- examples/test/pnm/SimpleDex.t.sol
+++ examples/test/pnm/SimpleDex.t.sol
@@ -6,17 +6,58 @@ import {SimpleDex} from "src/SimpleDex.sol";
 import {Token} from "src/Token.sol";
 
 contract SimpleDexTest is Agent {
+    address owner = address(0x1);
+    address lp = address(0x37);
+    address user = address(0x38);
+
     Token token;
     SimpleDex target;
 
     function setUp() public {
+        asAccountBegin(owner);
+        // Setup DEX with 10ETH and 10Token
         token = new Token();
+        setNativeBalance(address(token), 10);
         target = new SimpleDex(address(token));
-        target.init(10000);
+        target.init(10);
+
+        // Give all roles 10ETH and 10Token
+        setNativeBalance(lp, 10);
+        token.transfer(lp, 10);
+        setNativeBalance(user, 10);
+        token.transfer(user, 10);
+        setNativeBalance(address(this), 10);
+        token.transfer(address(this), 10);
+        asAccountEnd();
     }
 
-    function invariantProtocolBalanceShouldAlwaysBeSafe() external {
-        require(token.balanceOf(address(this)) >= 10000, "stolen");
-        console.log("Test success");
+    function actionLpDeposit(uint256 amount) public {
+        asAccountForNextCall(lp);
+        target.deposit{value: amount}();
+    }
+
+    function actionLpWithdraw(uint256 amount) public {
+        asAccountForNextCall(lp);
+        target.withdraw(amount);
+    }
+
+    function actionUserSwapFromEthToToken(uint256 amount) public {
+        asAccountForNextCall(user);
+        target.ethToToken{value: amount}();
+    }
+
+    function actionUserSwapFromTokenToEth(uint256 amount) public {
+        asAccountForNextCall(user);
+        target.tokenToEth(amount);
+    }
+
+    function invariantProtocolBalanceShouldAlwaysBeSafe() public {
+        address protocol = address(target);
+        assert(token.balanceOf(protocol) + protocol.balance >= 20);
+    }
+
+    function invariantHackerCanNotGainToken() public {
+        assert(address(this).balance <= 20);
+        assert(token.balanceOf(address(this)) <= 20);
     }
 }
