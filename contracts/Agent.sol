31b7c619044a897df9fc2facda06b0a312c8a3dc
tarafans <tarafans7@gmail.com>
2022-10-03 14:55:56 -0400

    v1.0.14

--- contracts/Agent.sol
+++ contracts/Agent.sol
@@ -14,7 +14,7 @@ struct Call {
 contract Agent is Test, ScriptEx {
     event RevertedCall(bytes4 selector, uint256 index);
 
-    bytes4 internal constant MAIN = bytes4(0x41414141);
+    bytes4 internal constant MAIN = bytes4(0xffffffff);
     bytes4 internal constant FALLBACK = bytes4(0x00000000);
     bytes4 internal constant ON_ERC721_RECEIVED = bytes4(0x150b7a02);
 
@@ -23,9 +23,11 @@ contract Agent is Test, ScriptEx {
     function main(Call[] calldata calls) external {
         uint256 i;
         for (i = 0; i < calls.length; i++) {
-            (bool succ, ) = calls[i].to.call(calls[i].callData);
-            if (succ) {
-                _testInvariant();
+            (bool success, bytes memory result) = calls[i].to.call(calls[i].callData);
+            if (success) {
+                testInvariant();
+            } else if (is_panic(result)) {
+                revert();
             } else {
                 emit RevertedCall(MAIN, i);
                 return;
@@ -50,7 +52,7 @@ contract Agent is Test, ScriptEx {
         invariantCalldata = _invariantCalldata;
     }
 
-    function _testInvariant() internal {
+    function testInvariant() internal {
         (bool success, bytes memory result) = address(this).call(invariantCalldata);
 
         if (!success) {
@@ -60,6 +62,14 @@ contract Agent is Test, ScriptEx {
             }
             revert(abi.decode(result, (string)));
         }
+    }
+
+    function is_panic(bytes memory result) internal pure returns (bool) {
+        bytes4 selector; 
+        assembly {
+            selector := mload(add(result, 0x20))
+        }
+        return selector == bytes4(0x4e487b71); // Panic(uint256)
     }
 
     function _callback(bytes4 selector, Call[] calldata calls) internal {
