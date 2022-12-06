d9856cdd026995b7b8c70958cea4018a90c0b1c4
tarafans <tarafans7@gmail.com>
2022-12-06 02:38:42 -0500

    New agent model

--- PTest.sol
+++ PTest.sol
@@ -3,15 +3,8 @@ pragma solidity >=0.6.0 <0.9.0;
 
 import "forge-std/Test.sol";
 import "./ScriptEx.sol";
-import "./Agent.sol";
 
 contract PTest is Test, ScriptEx {
-    Agent private _agent;
-
-    function getAgent() public returns (address) {
-        if (address(_agent) == address(0)) {
-            _agent = new Agent();
-        }
-        return address(_agent);
-    }
+    address constant agent =
+        address(bytes20(uint160(uint256(keccak256("pnm.agent")))));
 }
