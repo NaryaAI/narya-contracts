df03af2b3fbbce22269e147c922ccecfc5e6ab62
tarafans <tarafans7@gmail.com>
2023-01-09 01:41:59 -0500

    Update unprotected ownership

--- templates/UnprotectedOwnership.sol
+++ templates/UnprotectedOwnership.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import {PTest} from "../PTest.sol";
 
 abstract contract UnprotectedOwnershipTest is PTest {
-    address internal owner;
+    address internal oldOwner;
 
     // Test setup.
     function setUp() public {
@@ -15,16 +15,19 @@ abstract contract UnprotectedOwnershipTest is PTest {
     // Deploy the smart contracts to be tested.
     function deploy() public virtual;
 
+    // Initialize the states of the smart contracts.
+    function init() public virtual;
+
     // Return the current owner of your smart contract.
     function getOwner() public virtual returns (address);
 
-    // Initialize your smart contracts
-    function init() public virtual {
-        owner = getOwner();
+    // Records the initial owner of your smart contract.
+    function initOwner() public virtual {
+        oldOwner = getOwner();
     }
 
     // Check the owner of your smart contract is not changed
     function invariantUnprotectedOwnership() public virtual {
-        assert(getOwner() == owner);
+        assert(getOwner() == oldOwner);
     }
 }
