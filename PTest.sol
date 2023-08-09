4a6cb23f754ea4f93663ff3a0f0252d68e275a69
xiaohuajiao <hjxie04@gmail.com>
2023-08-09 22:49:59 +0800

    feat(multi-agent): support multi-agent

--- PTest.sol
+++ PTest.sol
@@ -8,8 +8,18 @@ contract PTest is Test, ScriptEx {
     uint256 private constant AGENT_PRIVATE_KEY =
         uint256(keccak256("pnm.agent"));
 
+    uint256 public agentCount = 1;
+
+    function setAgentCount(uint256 count) internal {
+        agentCount = count;
+    }
+
     function getAgent() internal returns (address) {
         return vm.addr(AGENT_PRIVATE_KEY);
+    }
+
+    function getAgent(uint256 index) internal returns (address) {
+        return vm.addr(AGENT_PRIVATE_KEY + index);
     }
 
     function getAgentKey() public pure returns (uint256) {
